359142c5e8f4cfd51302f97b3292abfbc243e493
nansnova <nancylesgj@outlook.com>
2023-10-11 21:17:26 +0000

    CP3 part 1

--- include/robot_info/agv_robot_info_class.h
+++ include/robot_info/agv_robot_info_class.h
@@ -1,16 +1,21 @@
-#pragma once
+#pragma once // This header is included only once in compilation.
 
-#include <robot_info/robot_info_class.h>
+#include <robot_info/robot_info_class.h> // Include the header file for the base RobotInfo class.
 
-class AGVRobotInfo : public RobotInfo {
+// Define a class named AgvInfo that inherits from the base RobotInfo class.
+class AgvInfo : public RobotInfo {
 public:
-  AGVRobotInfo(ros::NodeHandle &nh, const std::string &robot_description,
-               const std::string &serial_number, const std::string &ip_address,
-               const std::string &firmware_version,
-               const std::string &maximum_payload);
+  // Constructor for the AgvInfo class, inheriting from RobotInfo, and taking
+  // additional parameters.
+  AgvInfo(ros::NodeHandle &nh, const std::string &robot_description,
+          const std::string &serial_number, const std::string &ip_address,
+          const std::string &firmware_version,
+          const std::string &maximum_payload);
 
+  // Overridden method to publish data, as defined in the base class.
   void publish_data() override;
 
 private:
-  std::string maximum_payload;
+  std::string maximum_payload; // Member variable to store the maximum payload
+                               // information.
 };
--- src/agv_robot_info_class.cpp
+++ src/agv_robot_info_class.cpp
@@ -1,18 +1,19 @@
-#include <robot_info/agv_robot_info_class.h>
+#include <robot_info/agv_robot_info_class.h> // Include the header file for AGVRobotInfo class.
 
-AGVRobotInfo::AGVRobotInfo(ros::NodeHandle &nh,
-                           const std::string &robot_description,
-                           const std::string &serial_number,
-                           const std::string &ip_address,
-                           const std::string &firmware_version,
-                           const std::string &maximum_payload)
+// Constructor for the AgvInfo class, inheriting from RobotInfo.
+AgvInfo::AgvInfo(ros::NodeHandle &nh, const std::string &robot_description,
+                 const std::string &serial_number,
+                 const std::string &ip_address,
+                 const std::string &firmware_version,
+                 const std::string &maximum_payload)
     : RobotInfo(nh, robot_description, serial_number, ip_address,
-                firmware_version),
+                firmware_version), // Call the base class constructor.
       maximum_payload(maximum_payload) {}
 
-void AGVRobotInfo::publish_data() {
-  set_data_fields();
+// Implementation of the publish_data method for the AgvInfo class.
+void AgvInfo::publish_data() {
+  set_data_fields(); // Call the base class method to set common data fields.
   robot_info_msg.data_field_05 = "maximum_payload: " + maximum_payload;
 
-  robot_info_pub.publish(robot_info_msg);
+  robot_info_pub.publish(robot_info_msg); // Publish the populated message.
 }
